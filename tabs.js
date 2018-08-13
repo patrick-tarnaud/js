f39408ccc69519f81499470d314313aad967c1f0
ptarnaud <patrick.tarnaud@systeme-u.fr>
2018-08-13 14:03:07 +0200

    ajour activation

--- tabs.js
+++ tabs.js
@@ -1,16 +1,19 @@
-const afficherTabContent = function(e) {
-  console.log('afficherTabContent()')
-  const tabContentActive = document.querySelector('.tab-content.active')
-  console.log(tabContentActive)
-  tabContentActive.className='tab-content'
-  
-  /* const href = this.getAttribute('href')
-  const tabActive = document.querySelector('href') */
+const afficherTabContent = function(a) {
+  // div est le composant Onglet (tabs + tabs-content)
+  const div = a.parentNode.parentNode.parentNode
+
+  // le tab-content activé actuellement est desactivé
+  const tabContentActive = div.querySelector('.tab-content.active')
+  tabContentActive.classList.remove('active')
+
+  // le tab-content correspondant au a cliqué est activé
+  const href = a.getAttribute('href')
+  const tabContentAActiver = div.querySelector(href) 
+  tabContentAActiver.classList.add('active')
 }
 
-const tabs = document.querySelectorAll('.tabs li')
-for(let i=0; i<tabs.length; i++) {
-  let tab=tabs[i]
-  tab.addEventListener('click', function(e) { afficherTabContent(e) })
-
+const as = document.querySelectorAll('.tab a')
+for(let i=0; i<as.length; i++) {
+  let a=as[i]
+  a.addEventListener('click', function(e) { afficherTabContent(this) })
 }
